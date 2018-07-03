4a4bca8a06feabe9d7cc90cbe6618c8fc20d7054
Lucienlyl <352446302@qq.com>
2018-07-03 17:29:20 +0800

    lyl git try

--- app/src/main/java/com/example/pein/siamuniversity/Aboutus_EnglishLanguageCenterActivity.java
+++ app/src/main/java/com/example/pein/siamuniversity/Aboutus_EnglishLanguageCenterActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class Aboutus_EnglishLanguageCenterActivity extends AppCompatActivity {
 
     /*fxy first*/
+    /*lyl 2b*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
